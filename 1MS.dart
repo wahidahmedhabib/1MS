87b22825c64f077640484e1f6b9e5db2473d4177
wahidahmedhabib <wahidahmed221098@gmail.com>
2021-09-19 09:06:33 +0100

    Mark Sheet

---
+++ 1MS.dart
@@ -0,0 +1,41 @@
+
+DARTPAD MARKSHEET
+
+
+void main() {
+  String h="----MarkSheet----";
+  String name="Name=Abid";
+    String clas="Class=IX";
+  String sc="School=PAF Keit";
+  print(h);
+  print(name);
+  print(clas);
+  print(sc);
+ var a=76;
+  var b=78;
+  var c=88;
+  var d=98;
+  var e =81;
+  var ob= a+b+c+d+e;
+  print(ob);
+   var to=500;
+  print(to);
+  var per=ob*100/to;
+  print(per);
+  
+   if (per >= 33 && per <= 39) {
+    print("your Grade is E");
+  } else if (per >= 40 && per <= 49) {
+    print("Your Grade is D");
+  } else if (per >= 50 && per <= 59) {
+    print("Your Grade is C");
+  } else if (per >= 60 && per <= 69) {
+    print("Your Grade is B");
+  } else if (per >= 70 && per < 79) {
+    print(" Your Grade is A");
+  } else if (per > 80 && per <= 100) {
+    print("your Grade is A+");
+  } else
+    print("You are Fail");
+   }
+  
